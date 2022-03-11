06b21baf1419cb1f08eab86a56459aaf67c80b11
rbala <rbala@example.com>
2022-03-11 15:28:57 +0000

    utilizing classes

--- mtg/apimtg07.py
+++ mtg/apimtg07.py
@@ -12,15 +12,18 @@
 import requests
 
 # Define our "base" API
-API = "https://api.magicthegathering.io/v1/" # this will never change regardless of the lookup we perform
+API = "https://api.magicthegathering.io/v1/"# this will never change \
+       #regardless of the lookup we perform
 
 def main():
     """Run time code"""
 
-    setcode = input("What is the code of the set you are trying to lookup (see mtgsets.index for a list of codes)? ").lower()   # collect user input for MTG card set to lookup
+    setcode = input("What is the code of the set you are trying to lookup (see mtgsets.index \
+            for a list of codes)? ").lower()   # collect user input for MTG card set to lookup
 
     # create resp, which is our request object
-    resp = requests.get(f"{API}cards?set={setcode}")   # this "f" string reads: API + "cards/" + setcode
+    resp = requests.get(f"{API}cards?set={setcode}")   # this \
+            #"f" string reads: API + "cards/" + setcode
 
     # the .json() method will dump a JSON string into Pythonic data structures. COOL!
     # This is much easier than using the urllib.request library
